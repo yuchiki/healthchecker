d4d82ea408716479af4722d196458e840e62326c
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-03-05 19:15:39 +0900

    lint:fix で治るやつを直した (#1)

--- lib/constructs/status-storage.ts
+++ lib/constructs/status-storage.ts
@@ -1,15 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as constructs from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as constructs from 'constructs'
 
 export class StatusStorage extends constructs.Construct {
-  public readonly bucket: s3.Bucket;
+  public readonly bucket: s3.Bucket
 
   constructor(scope: constructs.Construct, id: string) {
-    super(scope, id);
+    super(scope, id)
 
     this.bucket = new s3.Bucket(this, 'StatusStorage', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
+    })
   }
 }
--- lib/healthchecker-stack.ts
+++ lib/healthchecker-stack.ts
@@ -1,10 +1,10 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
 
 export class HealthcheckerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
     const bucket = new Bucket(this, 'Bucket')
   }
 }
