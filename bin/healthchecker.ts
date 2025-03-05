11389331b2eadf7811279bb67e148cf3040fc178
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-03-05 10:22:34 +0000

    fix healthchecker.ts

--- bin/healthchecker.ts
+++ bin/healthchecker.ts
@@ -7,7 +7,7 @@ import { HealthcheckerStack } from '../lib/healthchecker-stack'
 
 const app = new cdk.App()
 Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
-new HealthcheckerStack(app, 'HealthcheckerStack', {
+new HealthcheckerStack(app, 'Healthchecker', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
