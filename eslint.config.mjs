7929c354724365a4ac4ed7fb9c8038a070037c55
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-03-05 10:02:44 +0000

    add tslint

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+
+
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import { FlatCompat } from '@eslint/eslintrc'
+import importPlugin from 'eslint-plugin-import';
+
+
+const compat = new FlatCompat()
+
+export default ts.config(
+  js.configs.recommended,
+  ...compat.extends("plugin:import/typescript"),
+  ...ts.configs.strictTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  stylistic.configs['recommended'],
+  {
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'object',
+            'type',
+            'index',
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+        },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    ignores: ['node_modules/', 'dist/', 'eslint.config.js'],
+  },
+)
